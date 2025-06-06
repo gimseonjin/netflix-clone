e501d3b82dab1fc2952f23e5556af90964d9bcd2
@gimseonjin <66009926+gimseonjin@users.noreply.github.com>
2025-06-06 11:02:02 +0900

    feat: UI Setting

--- netflix-frontend/src/App.js
+++ netflix-frontend/src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ProtectedRoute from "./ProtectedRoute";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import KakaoAuthRedirect from "./pages/KakaoAuthRedirect";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              Netplix
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ms-auto">
+                {!isLoggedIn ? (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/login">
+                        로그인
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/signup">
+                        회원가입
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <button className="btn btn-danger" onClick={handleLogout}>
+                        로그아웃
+                      </button>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route
+              path="/login"
+              element={<Login setIsLoggedIn={setIsLoggedIn} />}
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/login/oauth2/code/kakao"
+              element={<KakaoAuthRedirect />}
+            />
+
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ netflix-frontend/src/pages/KakaoAuthRedirect.js
@@ -0,0 +1,9 @@
+function KakaoAuthRedirect() {
+  return (
+    <div>
+      <h1>KakaoAuthRedirect</h1>
+    </div>
+  );
+}
+
+export default KakaoAuthRedirect;
